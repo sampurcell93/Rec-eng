c600b6018bc64e2f37396b61dc5258f188ae8455
Samuel Purcell <Samuel.Purcell@tufts.edu>
2013-12-09 18:59:16 -0500

    Added basic login form. no backend yet. lol

---
+++ README.md
@@ -0,0 +1,5 @@
+# Recommendation Engine
+## Goals
+The goal of this engine was to explore solutions like collaborative filtering, content based filtering, and a hybrid of the two. 
+We used MongoDB and Node.js in order to get up and running quickly, and have eschewed the use of a traditional login system here.
+We originally built an app in Rails, but this became unwieldy and the framework became more of a hindrance for testing than a helpful environment.
--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-var express   = require("express"),
-		    _ = require("underscore"),
-        app   = express(),
-        mongo = 'receng-sample',
-        db    = require("mongojs").connect(mongo,["posts", "users"]),
-        port
+var express = require("express"),
+	      _ = require("underscore"),
+        app = express(),
+      mongo = 'receng-sample',
+      objid = require("ObjectId"),
+      	 // The users table will also contain all of the posts, associated with each user. 
+      	 // Ideally we'd be using a relational model but we'll just hack it in for this project.
+         db = require("mongojs").connect(mongo,["users"]),
+       port, current_user = 0
 
 
+app.configure(function (){
+  app.use(express.logger('dev'));
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.static(__dirname + '/public'));
+});      
 
 var cc = function() { for (arg in arguments) console.log(arguments[arg]) }
 
@@ -13,11 +25,17 @@ app.listen((port = process.env.PORT || 5050), function() {
     console.log("Listening on " + port)
 })
 
-var Parser = function() {
+var parser = new (function() {
+
 	return {
 		getUserData: function(req, res, next) {
-			cc("getting user data")
-			next()
+			db.users.find({name: 'spurcell'}, function(err, result) {
+				if (!err) {
+					req.user = result
+					next()
+				}
+				else cc("FUCKING FUCK THIS PROJECT")
+			})
 		},
 		getFriendData: function(req, res, next) {
 			cc("getting friends")
@@ -28,10 +46,32 @@ var Parser = function() {
 			next()
 		}
 	}
-}
-
-var parser = new Parser()
+})
 
 app.get("/recommendations", parser.getUserData, parser.getFriendData, parser.getPostData, function(req,res) {
 	res.end("hello there")
+})
+
+app.get("/", function(req, res) {
+	res.render("createuser", {user: {
+		activities: ["swimming", "biking"]
+	}});
+})
+
+/* User REST API */
+app.post("/users", function(req,res) {
+	cc(req.body)
+	res.redirect("/")
+})
+
+app.get("/users/:name", function(req,res) {
+	db.users.find({name: req.params.name}, function(err, results) {
+		res.json(results)
+	})
+})
+
+app.get("/users", function(req,res) {
+	db.users.find({}, function(err, results) {
+		res.json(results)
+	})
 })
